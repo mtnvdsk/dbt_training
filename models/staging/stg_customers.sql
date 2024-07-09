fcbd650104515eb5564b11828eca1aa09039ba56
Tarun M <mtnvdsk@gmail.com>
2024-07-09 06:49:56 +0000

    other changes

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -1,7 +1,18 @@
-{{
-    config(
-        materialized='view',
-    )
-}}
+with source as (
 
-select * from {{ source('jaffle_shop', 'customers') }}
+    select * from {{ source('jaffle_shop', 'customers') }}
+
+),
+
+renamed as (
+
+    select
+        id as customer_id,
+        first_name,
+        last_name
+
+    from source
+
+)
+
+select * from renamed
